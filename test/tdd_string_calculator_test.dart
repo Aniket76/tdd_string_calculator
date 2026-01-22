4a2ae63366e5d65278b2809d6e0cf46b42b0a581
avishal <avishal@mcfadyen.com>
2026-01-22 11:04:37 +0530

    initial commit

---
+++ test/tdd_string_calculator_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdd_string_calculator/tdd_string_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
